312d85ad30a4c729cb9fb13d2ebe797e0008a85c
dbwcooper <dbwencookie@gmail.com>
2021-09-26 08:54:29 +0800

    feat: update sort-quick

--- src/components/leetcode/sort-quick.js
+++ src/components/leetcode/sort-quick.js
@@ -4,54 +4,6 @@ function swap(arr, i, j) {
   arr[j] = temp;
 }
 
-// // 快速排序
-// function partition(array, left = 0, right) {
-//   // 左指针
-//   left = typeof left === 'number' ? left : 0;
-//   // 右指针
-//   right = typeof right === 'number' ? right : array.length - 1;
-
-//   // 基准值
-//   let partition = array[left];
-//   let partitionIndex = left;
-//   while (left < right) {
-//     // 需要处理的情况
-//     if (array[right] < partition) {
-//       swap(array, left, right);
-//       partitionIndex = right;
-//     }
-//     if (array[left] > partition) {
-//       swap(array, left, right);
-//       partitionIndex = left;
-//     }
-
-//     // 判断 left 指针向右移动
-//     // 还是 right 指针向左移动
-//     if (partitionIndex === right) {
-//       left++;
-//     } else {
-//       right--;
-//     }
-//   }
-//   return left;
-// }
-
-// function quickSort(array, left = 0, right) {
-//   if (left >= right) return;
-//   // 左指针
-//   left = typeof left === 'number' ? left : 0;
-//   // 右指针
-//   right = typeof right === 'number' ? right : array.length - 1;
-
-//   let partitionIndex = partition(array, left, right);
-//   quickSort(array, left, partitionIndex - 1);
-//   quickSort(array, partitionIndex + 1, right);
-
-//   return array;
-// }
-
-// console.log(quickSort(arr));
-
 // 循环一次数组，并返回基准值的 index
 function partition(arr, left, right) {
   let value = arr[left];
